23880fa680c0eaf7be302d072ef0d7c661749752
Ak-Shaw <ayushshawz@gmail.com>
2022-08-28 01:49:38 +0530

    feat(getHello): replace Hello World! with initial welcome message for Twitter Bot

--- devops-twitter-bot/src/app.service.ts
+++ devops-twitter-bot/src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (
+      '<i>Twitter Bot</i> for the <b>DevOps Community</b>.' +
+      '<br/><br/>' +
+      'Kindly checkout the repository at <i><b><a href="https://github.com/rohitg00/devops-twitter-bot">rohitg00/devops-twitter-bot</a></b></i>'
+    );
   }
 }
